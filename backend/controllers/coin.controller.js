f948278e5f785e911e65a5c862d0deb59a26f22c
Alan Bond <noreply>
2021-01-18 09:37:21 -0800

    added coin controller

---
+++ backend/controllers/coin.controller.js
@@ -0,0 +1,20 @@
+//Todo: add in api dependencies
+//Todo: implement these controllers
+module.exports.findData = async (req,res) => {
+  throw new Error('Not implemented')
+}
+
+module.exports.list = (req,res) => {
+  throw new Error('Not implemented')
+}
+
+module.exports.add = async(req,res) => {
+  throw new Error('Not implemented')
+}
+
+module.exports.update = async(req,res) => {
+  throw new Error('Not implemented')
+}
+module.exports.delete = async(req,res)  => {
+  throw new Error('Not implemented')
+}
