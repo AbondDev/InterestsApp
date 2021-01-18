bc7ed53fb80a8c1dfdb47be3817ad7a2613566d0
Alan Bond <noreply>
2021-01-18 11:39:36 -0800

    implementing Listing functionality on backend

--- backend/controllers/coin.controller.js
+++ backend/controllers/coin.controller.js
@@ -1,11 +1,14 @@
 //Todo: add in api dependencies
 //Todo: implement these controllers
+const Coin = require('../models/coin.model.js')
+
 module.exports.fetch = async (req,res) => {
   throw new Error('Not implemented')
 }
 
-module.exports.list = (req,res) => {
-  throw new Error('Not implemented')
+module.exports.list = async (req,res) => {
+  const coins = await Coin.find({})
+  res.json(coins)
 }
 
 module.exports.add = async(req,res) => {
--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,23 @@ if(process.env.NODE_ENV !== "production") {
 
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose')
 const coinRoutes = require('./routes/coin.route.js');
 const server = express();
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/code-test'
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error: "));
+db.once('open', () => {
+  console.log('Database Connected')
+});
 
 server.use(cors())
 server.use(express.json())
@@ -15,7 +30,7 @@ server.use(express.urlencoded({extended: true}))
 
 server.use('/coins',coinRoutes)
 server.use('*', (req,res) => {
-  res.send(`Invalid url ${req.path}` )
+  res.send(`Invalid url ${req.originalUrl}` )
 })
 
 server.listen(process.env.PORT, () => {
