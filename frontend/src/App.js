4dac0844146e3b71f3439f247dd912ba972561eb
Alan Bond <noreply>
2021-01-19 10:43:40 -0800

    Added bootstrap and react-bootstrap dependencies. Implemented bootstrap in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,33 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
+import {Container, Navbar, Nav} from 'react-bootstrap'
 
 import CreateCoin from "./components/create-coin.component";
 import ShowCoin from "./components/show-coin.component";
 import CoinList from "./components/coins-list.component";
 
-import logo from "./logo.svg";
-
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-md navbar-light bg-light">
-            <a className="navbar-brand" href="/">
-              <img src={logo} width="30" height="30" alt="React Logo" />
-            </a>
-            <Link to="/" className="navbar-brand">Crypto Coin Interests App</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Coins</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Coin</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+        <Container>
+          <Navbar bg="light" expand="md">
+            <Navbar.Brand href="/">
+              Crypto Coin Interests App
+            </ Navbar.Brand >
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav" >
+              <Nav className = "mr-auto">
+                <Nav.Link href="/" >Coins</ Nav.Link >
+                <Nav.Link href="/create">Create Coin</ Nav.Link >
+              </Nav>
+            </ Navbar.Collapse >
+          </Navbar>
           <br/>
           <Route path="/" exact component={CoinList} />
           <Route path="/coin/:id" component={ShowCoin} />
           <Route path="/create" component={CreateCoin} />
-        </div>
+        </Container>
       </Router>
     );
   }
